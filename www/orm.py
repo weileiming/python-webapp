a68bd697a4b2caec5043cbc5e8c9ff9a8c461f6b
WillWei <2008weileiming@gmail.com>
2017-08-12 20:49:42 +0800

    Update code comments

--- www/orm.py
+++ www/orm.py
@@ -50,7 +50,7 @@ async def select(sql, args, size=None):
         return rs
 
 # INSERT、UPDATE、DELETE语句
-# 语句操作参数一样，定义通用执行函数
+# 3种SQL执行所需参数一样，定义通用执行函数
 async def execute(sql, args, autocommit=True):
     log(sql)
     async with __pool.get() as conn:
@@ -85,7 +85,8 @@ class Field(object):
         self.default = default
 
     def __str__(self):
-        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+        # 表名, 字段名: 字段类型
+        return '<%s, %s: %s>' % (self.__class__.__name__, self.name, self.column_type)
 
 # 映射数据库varchar类型
 class StringField(Field):
@@ -117,24 +118,32 @@ class TextField(Field):
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
 
-# 定义Model的元类
-# 读取映射信息
+# 定义Model的元类，继承自type
+# 为一个数据库表映射成一个封装的类做准备，读取具体子类的映射信息
 class ModelMetaclass(type):
     """docstring for ModelMetaclass"""
+    # __new__控制__init__的执行，所以在其执行之前
+    # cls: 代表要__init__的类，此参数在实例化时由Python解释器自动提供
+    # bases：代表继承父类的集合
+    # attrs：类的方法集合
     def __new__(cls, name, bases, attrs):
+        # 排除Model类本身
         if name == 'Model':
             return type.__new__(cls, name, bases, attrs)
+        # 获取表名
         tableName = attrs.get('__table__', None) or name
         logging.info('found model: %s (table: %s)' % (name, tableName))
-
+        # 获取所有的Field和主键名
         mappings = dict()
         fields = []
         primaryKey = None
+        # key表示字段名，value表示属性
         for key, value in attrs.items():
             if isinstance(value, Field):
                 logging.info('found mapping: %s ==> %s' % (key, value))
                 mappings[key] = value
                 if value.primary_key:
+                    # 主键
                     if primaryKey:
                         raise StandardError('Duplicate primary key for field: %s' % key)
                     primaryKey = key
@@ -146,17 +155,23 @@ class ModelMetaclass(type):
             attrs.pop(key)
 
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        # 保存属性和列的映射关系
         attrs['__mappings__'] = mappings
+        # 表名
         attrs['__table__'] = tableName
+        # 主键属性名
         attrs['__primaty_key__'] = primaryKey
+        # 除主键外的属性名
         attrs['__fields__'] = fields
+        # 构造默认的CRUD操作语句
         attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
         attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
         attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
 
-
+# 定义所有ORM映射的基类
+# Model类的子类映射数据库表
 class Model(dict, metaclass=ModelMetaclass):
     """docstring for Model"""
     def __init__(self, **kw):
@@ -184,6 +199,8 @@ class Model(dict, metaclass=ModelMetaclass):
                 setattr(self, key, value)
         return value
 
+    # 类方法
+    # 根据where条件查找
     @classmethod
     async def findAll(cls, where=None, args=None, **kw):
         ' find objects by where clause. '
@@ -211,6 +228,8 @@ class Model(dict, metaclass=ModelMetaclass):
         rs = await select(' '.join(sql), args)
         return [cls(**r) for r in rs]
 
+    # 类方法
+    # 根据where条件查找，但返回整数
     @classmethod
     async def findNumber(cls, selectField, where=None, args=None):
         ' find number by select and where. '
@@ -223,6 +242,7 @@ class Model(dict, metaclass=ModelMetaclass):
             return None
         return rs[0]['_num_']
 
+    # 类方法
     @classmethod
     async def fnd(cls, pk):
         ' find object by primary key. '
@@ -231,6 +251,8 @@ class Model(dict, metaclass=ModelMetaclass):
             return None
         return cls(**rs[0])
 
+    # 实例方法
+    # 保存
     async def save(self):
         args = list(map(self.getValueOrDefault, self.__fields__))
         args.append(self.getValueOrDefault(self.__primaty_key__))
@@ -238,6 +260,8 @@ class Model(dict, metaclass=ModelMetaclass):
         if rows != 1:
             logging.warn('failed to insert record: affected rows: %s' % rows)
 
+    # 实例方法
+    # 更新
     async def update(self):
         args = list(map(self.getValue, self.__fields__))
         args.append(self.getValue(self.__primaty_key__))
@@ -245,6 +269,8 @@ class Model(dict, metaclass=ModelMetaclass):
         if rows != 1:
             logging.warn('failed to update by primary key: affected rows: %s' % rows)
 
+    # 实例方法
+    # 删除
     async def remove(self):
         args = [self.getValue(self.__primaty_key__)]
         rows = await execute(self.__delete__, args)
