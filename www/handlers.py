05262da1dcd2266f67d8d738a7cf7fcbb375ac11
WillWei <2008weileiming@gmail.com>
2017-08-22 14:53:16 +0800

    Add MVC test

--- www/handlers.py
+++ www/handlers.py
@@ -5,4 +5,17 @@
 url handlers
 """
 
+import asyncio
+from coroweb import get, post
+from models import User, Comment, Blog, next_id
+
 __author__ = 'Will Wei'
+
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
